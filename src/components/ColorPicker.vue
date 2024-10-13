cdbb6aeea8b8602e121216c83a2bc1de5381f58b
Dennis Haas <haasde@netrtl.de>
2024-10-13 18:25:57 +0200

    add .env to gitignore fix colorPicker component

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,31 +1,16 @@
 <template>
   <ul class="blanklist color-picker">
-    <template v-for="color in accessibleColors" :key="color.color">
-      <li
-        :class="{
-          'is-selected': onReload
-            ? selectedColor.color === color.color
-            : userImageBgColor === color.color,
-        }"
-      >
+    <template v-for="(color) in accessibleColors" :key="color.color">
+      <li :class="{'is-selected' : onReload ? selectedColor.color === color.color : userImageBgColor === color.color}">
         <button
-          type="button"
-          @click="handleColorPicker(color)"
-          :style="
-            !color.image
-              ? { '--color-picker': `var(${color.color})` }
-              : { '--background-image-path': `url(${color.color})` }
-          "
-          :title="`${color.name} als Profilhintergrundfarbe auswählen`"
-        >
-          <span
-            :class="[
-              'color-picker__picker-circle',
-              { 'color-picker__picker-circle-image': color.image },
-            ]"
-          >
-          </span>
-          <span>{{ color.name }}</span>
+            type="button"
+            @click="handleColorPicker(color)"
+            :style="!color.image ? {'--color-picker': `var(${color.color})`} : {'--background-image-path': `url(${color.color})`}"
+            :title="`${color.name} als Profilhintergrundfarbe auswählen`">
+            <span
+              :class="['color-picker__picker-circle', { 'color-picker__picker-circle-image' : color.image }]">
+            </span>
+            <span>{{ color.name }}</span>
         </button>
       </li>
     </template>
@@ -33,11 +18,11 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, onMounted, ref } from 'vue'
-import { useUser } from '@/composables/useUser.ts'
-import { UserData } from '@/interface'
+import {reactive, onMounted, ref} from 'vue';
+import {useUser} from "@/composables/useUser.ts";
+import {UserData} from "@/interface";
 
-const { getUserByID, selectedUser } = useUser()
+const {getUserByID, selectedUser} = useUser()
 const getSelectedUser = ref<UserData | null>(null)
 const userImageBgColor = ref('')
 const onReload = ref(false)
@@ -45,77 +30,72 @@ const onReload = ref(false)
 const props = defineProps({
   selectedColor: {
     type: Object,
-    default: () => [],
+    default: () => ({}),
   },
   id: {
     type: Number,
-    default: 0,
-  },
-})
+    default: 0
+  }
+});
 
-const emit = defineEmits(['update:selectedColor'])
+const emit = defineEmits(['update:selectedColor']);
 const accessibleColors = reactive([
   {
     color: '--warning-light',
-    name: 'Gelb',
+    name: 'Gelb'
   },
   {
     color: '--blue-primary',
-    name: 'Blau',
+    name: 'Blau'
   },
   {
     color: '--blue-dark-gray',
-    name: 'Dunkelblau',
+    name: 'Dunkelblau'
   },
   {
     color: '--primary',
-    name: 'Rot',
+    name: 'Rot'
   },
   {
     color: '--success-mint',
-    name: 'Minzgrün',
+    name: 'Minzgrün'
   },
   {
     color: '--purple',
-    name: 'Lila',
+    name: 'Lila'
   },
   {
     name: 'Derbysieger',
     color: '/images/hsv.svg',
-    image: true,
+    image: true
   },
   {
     name: 'Kiezkicker',
     color: '/images/pauli.svg',
-    image: true,
+    image: true
   },
   {
     name: 'Norderstedt',
     color: '/images/sh.svg',
-    image: true,
-  },
-])
+    image: true
+  }
+]);
 
-const handleColorPicker = (color: {
-  color: string
-  name: string
-  image?: boolean
-}) => {
-  onReload.value = true
-  emit('update:selectedColor', color.color)
-}
+const handleColorPicker = (color: { color: string; name: string; image?: boolean }) => {
+  onReload.value = true;
+  emit('update:selectedColor', color); // Hier das ganze color Objekt übergeben
+};
 
 onMounted(async () => {
-  await getUserByID(props.id)
-  getSelectedUser.value = selectedUser.value
+  await getUserByID(props.id);
+  getSelectedUser.value = selectedUser.value;
 
-  if (getSelectedUser.value) {
-    userImageBgColor.value = getSelectedUser.value.userImage.bgColor
+  if (getSelectedUser.value && getSelectedUser.value.userImage) {
+    userImageBgColor.value = getSelectedUser.value.userImage.bgColor;
   } else {
-    // Handle case when user data is not found (e.g., set a default value)
-    userImageBgColor.value = '' // or some default color
+    userImageBgColor.value = '';
   }
-})
+});
 </script>
 
 <style scoped lang="scss">
