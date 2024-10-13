ba59b13220e5b593d1725bdf423efa963ee675bd
Dennis Haas <haasde@netrtl.de>
2024-10-13 17:04:40 +0200

    update branch

--- backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// SQLite-Verbindung einrichten
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database/database.sqlite'
-});
-
-module.exports = sequelize;
