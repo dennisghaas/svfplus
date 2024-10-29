cd8cd9ccc37966993647bd21d8e485cf8cc27cf9
Dennis Haas <haasde@netrtl.de>
2024-10-29 11:05:02 +0100

    update branch

--- src/Pages/LineUpView.vue
+++ src/Pages/LineUpView.vue
@@ -1,9 +1,13 @@
 <template>
+  <LineUpField drop-player="" positions=""
+               <LineUp/>
 
 </template>
 
 <script setup lang="ts">
 
+import LineUpField from "@/components/LineUp/LineUpField.vue";
+import LineUp from "@/components/LineUp/LineUp.vue";
 </script>
 
 <style scoped lang="scss">
